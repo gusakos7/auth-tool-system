1e4248ecdf077d5e36ed7f0d35034119897670be
raftkon <raftkon@raftkon>
2025-04-07 17:39:15 +0300

    initial commit

---
+++ scripts/logs.sh
@@ -0,0 +1,1 @@
+docker compose -f docker-compose.yml -f compose.traefik.yml logs -f
---
+++ scripts/start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# This script is used to start the Docker containers for the application.
+# It uses Docker Compose to bring up the containers defined in the compose.yml and compose.traefik.yml files.
+
+docker compose -f compose.traefik.yml -f compose.yml -f compose.api-dev.yml up -d --build
